1429fda265f469347e742c527d84fea19b2e4f65
Felipe Rivas <rivascf@gmail.com>
2024-11-19 22:30:49 -0600

    Updated logger script

--- voice_assistant/utils/logger.py
+++ voice_assistant/utils/logger.py
@@ -13,8 +13,12 @@ import datetime
 
 def configure_logging():
     """Sets up logging configuration for the application."""
+    # format type 01: [INFO] [1731956279.407480733] [service_server]: Server initialized...
+    # format type 02:
+    # [DEBUG] [launch]: processing event: '<launch.events.process.process_started.ProcessStarted object at 0x7bee6cf47430>'
     logging.basicConfig(
         filename=f'./logs/voice_assistant_{datetime.datetime.now():%d%m%y}.log',
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        #format='%(asctime)s - %(levelname)s - %(message)s'
+        format='[%(levelname)s] [%(asctime)s]: %(message)s'
     )
